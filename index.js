4db69da8d1c0753b27f55218fe2be75489f874d8
shaykot43500 <hossainshaykot@gmail.com>
2023-03-15 23:48:05 +0600

    added new id

--- index.js
+++ index.js
@@ -46,8 +46,9 @@ startQuiz.addEventListener("click", () => {
 // All quiz data fetched from json
 const loadQuiz = async () => {
   const res = await fetch("./data/quiz.json");
-  const data = await res.json;
+  const data = await res.json();
   quizData = data;
+  // console.log(data)
   displayQuiz(data);
 };
 
@@ -59,12 +60,13 @@ const displayQuiz = (data) => {
   }
 
   data.forEach((quiz, i) => {
+    console.log(quiz)
     quizContainer.innerHTML += `<div class="m-3 py-3 px-4 shadow-sm rounded">
   <div class="flex items-center">
     <div class="h-8 w-8 bg-green-300 rounded-full flex justify-center items-center text-green-800 mr-3">
       ${i + 1}
     </div>
-    <p class="text-gray-800 text-sm">${quiz.quetion}</p>
+    <p class="text-gray-800 text-sm">${quiz.question}</p>
   </div>
   <div class="grid grid-cols-2 gap-4 mt-5">
     ${displayQuizOptions(quiz.options, i)}
@@ -75,6 +77,7 @@ const displayQuiz = (data) => {
 
 // EventListener for quiz submit button
 document.querySelector("#submit").addEventlistener("click", () => {
+  console.log('submit')
   if (answers.length < 6) {
     return;
   }
